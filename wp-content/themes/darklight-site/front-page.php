a875c22186024e33aea62b6e40f7593ef4b9d99a
Ryan Ward <wardy547@Gmail.com>
2016-05-19 13:51:02 +0100

    made changes to file layout
    i added lanes and font awesome also made changes to my front-page.php

--- wp-content/themes/darklight-site/front-page.php
+++ wp-content/themes/darklight-site/front-page.php
@@ -3,15 +3,44 @@
 * Template Name: front page
 */
 get_header();
-
 $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail')[0]
 
 ?>
-
+<div class="body">
+<div class= "container">
+  <div class= "row">
+    <div class= "col-6">
 <section class="banner">
   <img src="<?php echo $image; ?>">
 </section>
-<h1>hello</h1>
+</div>
+<div class= "col-6 head">
+  <h1>Darklight</h1>
+  <br>
+  <p>Not Your Usual Photography Company</p>
+  <br>
+  <nav>
+    <ul class"header__links">
+      <li>
+        <a class="header__link-item" href="#"><i class="fa fa-facebook"></i></a>
+        </li>
+        <li>
+          <a class="header__link-item" href="#"><i class="fa fa-twitter"></i></a>
+        </li>
+        <li>
+          <a class="header__link-item" href="#"><i class="fa fa-instagram"></i></a>
+        </li>
+        <li>
+          <a class="header__link-item" href="#"><i class="fa fa-envelope-o"></i></a>
+        </li>
+      </ul>
+    </nav>
+</div>
+</div>
+</div>
+</div>
+
+
 
 
 
--- wp-content/themes/darklight-site/functions.php
+++ wp-content/themes/darklight-site/functions.php
@@ -7,6 +7,8 @@ function style_script_includes() {
   wp_enqueue_script('jquery');
   wp_register_style('theme_style', $stylesheet_uri);
   wp_enqueue_style('theme_style');
+  wp_register_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css', '', '', false);
+  wp_enqueue_style('font-awesome');
 }
 add_action('wp_enqueue_scripts', 'style_script_includes');
 
